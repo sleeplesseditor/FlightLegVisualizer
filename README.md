8106bc662e3299c141299f34697a36472a6766a5
sleeplesseditor <josh@bryson.me.uk>
2019-03-30 18:09:53 +0000

    Initial setup

---
+++ README.md
@@ -0,0 +1,3 @@
+#FlightLegVisualizer
+
+A visualization of flight legs built using React, Next, and Mapbox.
---
+++ pages/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Index = () => {
+    return <div className="root"> Here Be Flights</div>;
+};
+
+export default Index;
